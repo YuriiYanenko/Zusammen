619d7fc97da01aab13f1a80d524e4e407c3109c7
valeriya <kovalenkolera11@gmail.com>
2024-01-26 00:43:00 +0200

    changes

---
+++ Zusammen/Views/Home/user.cs
@@ -0,0 +1,14 @@
+namespace Zusammen.Views.Home;
+
+public class user
+{
+    public int Id { get; set; }
+    public string Username { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string Status { get; set; }
+    public string Prof_descr { get; set; }
+    public List<int> Rooms { get; set; }
+    public string Prof_image { get; set; }
+    
+}
