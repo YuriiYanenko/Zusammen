8e287cced5acc5875bf9488b6de15979b38a850b
Mara <bezpalkomasha46@gmail.com>
2024-01-31 23:30:30 +0200

    куку

--- Zusammen/obj/Debug/net8.0/Zusammen.AssemblyInfo.cs
+++ Zusammen/obj/Debug/net8.0/Zusammen.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Zusammen")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+912d6152697db4311e7ba480a97e2383561ca19e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59935e28f653dedb3749a4512f79f3b57985568e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Zusammen")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Zusammen")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
