fc9a50fb0e0b83af78d72d71a48d108411562a76
valeriya <kovalenkolera11@gmail.com>
2024-02-01 00:18:09 +0200

    add changes

--- Zusammen/obj/Debug/net8.0/Zusammen.AssemblyInfo.cs
+++ Zusammen/obj/Debug/net8.0/Zusammen.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Zusammen")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59935e28f653dedb3749a4512f79f3b57985568e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+854bee9d3cdf5d96177e894d818ba225738ff650")]
 [assembly: System.Reflection.AssemblyProductAttribute("Zusammen")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Zusammen")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
